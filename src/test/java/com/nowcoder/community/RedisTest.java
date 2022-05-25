fa6d67a41530bc75a6148306da27a01318a0d73c
weixin <weixin1128211@163.com>
2022-05-25 17:56:32 +0800

    redis

---
+++ src/test/java/com/nowcoder/community/RedisTest.java
@@ -0,0 +1,85 @@
+package com.nowcoder.community;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testString(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void hashTest(){
+        String redisKey = "test:user";
+
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "weixin");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    @Test
+    public void testKeys(){
+        redisTemplate.delete("test:user");
+
+        System.out.println(redisTemplate.hasKey("test:user"));
+
+        redisTemplate.expire("test:students",10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testBoundOperations(){
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    @Test
+    public void testTrans(){
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "test:tx";
+                redisOperations.multi();
+                redisOperations.opsForSet().add(redisKey,"aaa");
+                redisOperations.opsForSet().add(redisKey,"bbb");
+                redisOperations.opsForSet().add(redisKey,"ccc");
+
+                System.out.println(redisOperations.opsForSet().members(redisKey));
+
+                return redisOperations.exec();
+            }
+        });
+        System.out.println(obj);
+    }
+}
